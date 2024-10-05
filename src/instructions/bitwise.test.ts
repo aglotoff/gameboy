e1cc5fa1228224abc99f815a56570a08eb57aea7
Andrey Glotov <andrei.glotoff@gmail.com>
2024-10-05 12:46:25 +0300

    feat: cb prefix instructions

--- src/instructions/bitwise.test.ts
+++ src/instructions/bitwise.test.ts
@@ -1,6 +1,12 @@
 import { describe, expect, test as baseTest } from "vitest";
 
-import { Flag, InterruptFlags, Register, RegisterFile } from "../cpu";
+import {
+  Flag,
+  InterruptFlags,
+  Register,
+  RegisterFile,
+  RegisterPair,
+} from "../cpu";
 import { Memory } from "../memory";
 
 import { InstructionCtx } from "./lib";
@@ -9,6 +15,28 @@ import {
   rotateRightCircularAccumulator,
   rotateLeftAccumulator,
   rotateRightAccumulator,
+  rotateLeftCircularRegister,
+  rotateLeftCircularIndirectHL,
+  rotateRightCircularRegister,
+  rotateRightCircularIndirectHL,
+  rotateLeftRegister,
+  rotateLeftIndirectHL,
+  rotateRightRegister,
+  rotateRightIndirectHL,
+  shiftLeftArithmeticRegister,
+  shiftLeftArithmeticIndirectHL,
+  shiftRightArithmeticRegister,
+  shiftRightArithmeticIndirectHL,
+  shiftRightLogicalRegister,
+  shiftRightLogicalIndirectHL,
+  swapNibblesRegister,
+  swapNibblesIndirectHL,
+  testBitRegister,
+  testBitIndirectHL,
+  resetBitRegister,
+  resetBitIndirectHL,
+  setBitRegister,
+  setBitIndirectHL,
 } from "./bitwise";
 
 const test = baseTest.extend({
@@ -70,5 +98,321 @@ describe("Rotate, shift, and bit operation instructions", () => {
     expect(ctx.regs.isFlagSet(Flag.Z)).toBe(false);
     expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
     expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("RLC r", ({ ctx }) => {
+    ctx.regs.write(Register.B, 0x85);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    rotateLeftCircularRegister(ctx, Register.B);
+
+    expect(ctx.regs.read(Register.B)).toBe(0x0b);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("RLC (HL)", ({ ctx }) => {
+    ctx.memory.write(0x8ac5, 0x00);
+    ctx.regs.writePair(RegisterPair.HL, 0x8ac5);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    rotateLeftCircularIndirectHL(ctx);
+
+    expect(ctx.memory.read(0x8ac5)).toBe(0x00);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("RRC r", ({ ctx }) => {
+    ctx.regs.write(Register.C, 0x01);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    rotateRightCircularRegister(ctx, Register.C);
+
+    expect(ctx.regs.read(Register.C)).toBe(0x80);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("RRC (HL)", ({ ctx }) => {
+    ctx.memory.write(0x8ac5, 0x00);
+    ctx.regs.writePair(RegisterPair.HL, 0x8ac5);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    rotateRightCircularIndirectHL(ctx);
+
+    expect(ctx.memory.read(0x8ac5)).toBe(0x00);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("RL r", ({ ctx }) => {
+    ctx.regs.write(Register.L, 0x80);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    rotateLeftRegister(ctx, Register.L);
+
+    expect(ctx.regs.read(Register.L)).toBe(0x00);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("RL (HL)", ({ ctx }) => {
+    ctx.memory.write(0x8ac5, 0x11);
+    ctx.regs.writePair(RegisterPair.HL, 0x8ac5);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    rotateLeftIndirectHL(ctx);
+
+    expect(ctx.memory.read(0x8ac5)).toBe(0x22);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("RR r", ({ ctx }) => {
+    ctx.regs.write(Register.A, 0x01);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    rotateRightRegister(ctx, Register.A);
+
+    expect(ctx.regs.read(Register.A)).toBe(0x00);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("RR (HL)", ({ ctx }) => {
+    ctx.memory.write(0x8ac5, 0x8a);
+    ctx.regs.writePair(RegisterPair.HL, 0x8ac5);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    rotateRightIndirectHL(ctx);
+
+    expect(ctx.memory.read(0x8ac5)).toBe(0x45);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("SLA r", ({ ctx }) => {
+    ctx.regs.write(Register.D, 0x80);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    shiftLeftArithmeticRegister(ctx, Register.D);
+
+    expect(ctx.regs.read(Register.D)).toBe(0x00);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("SLA (HL)", ({ ctx }) => {
+    ctx.memory.write(0x8ac5, 0xff);
+    ctx.regs.writePair(RegisterPair.HL, 0x8ac5);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    shiftLeftArithmeticIndirectHL(ctx);
+
+    expect(ctx.memory.read(0x8ac5)).toBe(0xfe);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("SRA r", ({ ctx }) => {
+    ctx.regs.write(Register.A, 0x8a);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    shiftRightArithmeticRegister(ctx, Register.A);
+
+    expect(ctx.regs.read(Register.A)).toBe(0xc5);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("SRA (HL)", ({ ctx }) => {
+    ctx.memory.write(0x8ac5, 0x01);
+    ctx.regs.writePair(RegisterPair.HL, 0x8ac5);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    shiftRightArithmeticIndirectHL(ctx);
+
+    expect(ctx.memory.read(0x8ac5)).toBe(0x00);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("SRL r", ({ ctx }) => {
+    ctx.regs.write(Register.A, 0x01);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    shiftRightLogicalRegister(ctx, Register.A);
+
+    expect(ctx.regs.read(Register.A)).toBe(0x00);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("SRL (HL)", ({ ctx }) => {
+    ctx.memory.write(0x8ac5, 0xff);
+    ctx.regs.writePair(RegisterPair.HL, 0x8ac5);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    shiftRightLogicalIndirectHL(ctx);
+
+    expect(ctx.memory.read(0x8ac5)).toBe(0x7f);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("SWAP r", ({ ctx }) => {
+    ctx.regs.write(Register.A, 0x00);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    swapNibblesRegister(ctx, Register.A);
+
+    expect(ctx.regs.read(Register.A)).toBe(0x00);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(true);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  test("SWAP (HL)", ({ ctx }) => {
+    ctx.memory.write(0x8ac5, 0xf0);
+    ctx.regs.writePair(RegisterPair.HL, 0x8ac5);
+    ctx.regs.setFlag(Flag.CY, false);
+
+    swapNibblesIndirectHL(ctx);
+
+    expect(ctx.memory.read(0x8ac5)).toBe(0x0f);
+    expect(ctx.regs.isFlagSet(Flag.CY)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.Z)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.H)).toBe(false);
+    expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+  });
+
+  describe("BIT b, r", () => {
+    test("BIT 7, A", ({ ctx }) => {
+      ctx.regs.write(Register.A, 0x80);
+
+      testBitRegister(ctx, 7, Register.A);
+
+      expect(ctx.regs.isFlagSet(Flag.Z)).toBe(false);
+      expect(ctx.regs.isFlagSet(Flag.H)).toBe(true);
+      expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+    });
+
+    test("BIT 4, L", ({ ctx }) => {
+      ctx.regs.write(Register.L, 0xef);
+
+      testBitRegister(ctx, 4, Register.L);
+
+      expect(ctx.regs.isFlagSet(Flag.Z)).toBe(true);
+      expect(ctx.regs.isFlagSet(Flag.H)).toBe(true);
+      expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+    });
+  });
+
+  describe("BIT b, (HL)", () => {
+    test("BIT 0, (HL)", ({ ctx }) => {
+      ctx.memory.write(0x8ac5, 0xfe);
+      ctx.regs.writePair(RegisterPair.HL, 0x8ac5);
+
+      testBitIndirectHL(ctx, 0);
+
+      expect(ctx.regs.isFlagSet(Flag.Z)).toBe(true);
+      expect(ctx.regs.isFlagSet(Flag.H)).toBe(true);
+      expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+    });
+
+    test("BIT 1, (HL)", ({ ctx }) => {
+      ctx.memory.write(0x8ac5, 0xfe);
+      ctx.regs.writePair(RegisterPair.HL, 0x8ac5);
+
+      testBitIndirectHL(ctx, 1);
+
+      expect(ctx.regs.isFlagSet(Flag.Z)).toBe(false);
+      expect(ctx.regs.isFlagSet(Flag.H)).toBe(true);
+      expect(ctx.regs.isFlagSet(Flag.N)).toBe(false);
+    });
+  });
+
+  describe("RES b, r", () => {
+    test("RES 7, A", ({ ctx }) => {
+      ctx.regs.write(Register.A, 0x80);
+
+      resetBitRegister(ctx, 7, Register.A);
+
+      expect(ctx.regs.read(Register.A)).toBe(0x00);
+    });
+
+    test("RES 1, L", ({ ctx }) => {
+      ctx.regs.write(Register.L, 0x3b);
+
+      resetBitRegister(ctx, 1, Register.L);
+
+      expect(ctx.regs.read(Register.L)).toBe(0x39);
+    });
+  });
+
+  test("RES b, (HL)", ({ ctx }) => {
+    ctx.memory.write(0x8ac5, 0xff);
+    ctx.regs.writePair(RegisterPair.HL, 0x8ac5);
+
+    resetBitIndirectHL(ctx, 3);
+
+    expect(ctx.memory.read(0x8ac5)).toBe(0xf7);
+  });
+
+  describe("SET b, r", () => {
+    test("SET 2, A", ({ ctx }) => {
+      ctx.regs.write(Register.A, 0x80);
+
+      setBitRegister(ctx, 2, Register.A);
+
+      expect(ctx.regs.read(Register.A)).toBe(0x84);
+    });
+
+    test("SET 7, L", ({ ctx }) => {
+      ctx.regs.write(Register.L, 0x3b);
+
+      setBitRegister(ctx, 7, Register.L);
+
+      expect(ctx.regs.read(Register.L)).toBe(0xbb);
+    });
+  });
+
+  test("SET b, (HL)", ({ ctx }) => {
+    ctx.memory.write(0x8ac5, 0x00);
+    ctx.regs.writePair(RegisterPair.HL, 0x8ac5);
+
+    setBitIndirectHL(ctx, 3);
+
+    expect(ctx.memory.read(0x8ac5)).toBe(0x08);
   });
 });
--- src/instructions/bitwise.ts
+++ src/instructions/bitwise.ts
@@ -1,48 +1,408 @@
-import { Flag, Register } from "../cpu";
+import { Flag, Register, RegisterPair } from "../cpu";
 import { InstructionCtx } from "./lib";
 
+function rotateLeftCircular(value: number) {
+  return {
+    result: ((value << 1) | (value >> 7)) & 0xff,
+    carry: (value & 0x80) != 0,
+  };
+}
+
+function rotateLeft(value: number, carry: boolean) {
+  return {
+    result: ((value << 1) & 0xff) | (carry ? 0x01 : 0x00),
+    carry: (value & 0x80) != 0,
+  };
+}
+
+function rotateRightCircular(value: number) {
+  return {
+    result: ((value >> 1) | (value << 7)) & 0xff,
+    carry: (value & 0x01) != 0,
+  };
+}
+
+function rotateRight(value: number, carry: boolean) {
+  return {
+    result: ((value >> 1) & 0xff) | (carry ? 0x80 : 0x00),
+    carry: (value & 0x01) != 0,
+  };
+}
+
 export function rotateLeftCircularAccumulator(ctx: InstructionCtx) {
-  const value = ctx.regs.read(Register.A);
-  ctx.regs.write(Register.A, ((value << 1) | (value >> 7)) & 0xff);
+  const { result, carry } = rotateLeftCircular(ctx.regs.read(Register.A));
+
+  ctx.regs.write(Register.A, result);
   ctx.regs.setFlag(Flag.Z, false);
   ctx.regs.setFlag(Flag.N, false);
   ctx.regs.setFlag(Flag.H, false);
-  ctx.regs.setFlag(Flag.CY, (value & 0x80) != 0);
+  ctx.regs.setFlag(Flag.CY, carry);
+
   return 4;
 }
 
 export function rotateRightCircularAccumulator(ctx: InstructionCtx) {
-  const value = ctx.regs.read(Register.A);
-  ctx.regs.write(Register.A, ((value >> 1) | (value << 7)) & 0xff);
+  const { result, carry } = rotateRightCircular(ctx.regs.read(Register.A));
+
+  ctx.regs.write(Register.A, result);
   ctx.regs.setFlag(Flag.Z, false);
   ctx.regs.setFlag(Flag.N, false);
   ctx.regs.setFlag(Flag.H, false);
-  ctx.regs.setFlag(Flag.CY, (value & 0x01) != 0);
+  ctx.regs.setFlag(Flag.CY, carry);
+
   return 4;
 }
 
 export function rotateLeftAccumulator(ctx: InstructionCtx) {
-  const value = ctx.regs.read(Register.A);
-  ctx.regs.write(
-    Register.A,
-    ((value << 1) & 0xff) | (ctx.regs.isFlagSet(Flag.CY) ? 1 : 0)
+  const { result, carry } = rotateLeft(
+    ctx.regs.read(Register.A),
+    ctx.regs.isFlagSet(Flag.CY)
   );
+
+  ctx.regs.write(Register.A, result);
   ctx.regs.setFlag(Flag.Z, false);
   ctx.regs.setFlag(Flag.N, false);
   ctx.regs.setFlag(Flag.H, false);
-  ctx.regs.setFlag(Flag.CY, (value & 0x80) != 0);
+  ctx.regs.setFlag(Flag.CY, carry);
+
   return 4;
 }
 
 export function rotateRightAccumulator(ctx: InstructionCtx) {
-  const value = ctx.regs.read(Register.A);
-  ctx.regs.write(
-    Register.A,
-    ((value >> 1) & 0xff) | (ctx.regs.isFlagSet(Flag.CY) ? 0x80 : 0)
+  const { result, carry } = rotateRight(
+    ctx.regs.read(Register.A),
+    ctx.regs.isFlagSet(Flag.CY)
   );
+
+  ctx.regs.write(Register.A, result);
   ctx.regs.setFlag(Flag.Z, false);
   ctx.regs.setFlag(Flag.N, false);
   ctx.regs.setFlag(Flag.H, false);
-  ctx.regs.setFlag(Flag.CY, (value & 0x1) != 0);
+  ctx.regs.setFlag(Flag.CY, carry);
+
   return 4;
+}
+
+export function rotateLeftCircularRegister(
+  ctx: InstructionCtx,
+  register: Register
+) {
+  const { result, carry } = rotateLeftCircular(ctx.regs.read(register));
+
+  ctx.regs.write(register, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, carry);
+
+  return 8;
+}
+
+export function rotateLeftCircularIndirectHL(ctx: InstructionCtx) {
+  const address = ctx.regs.readPair(RegisterPair.HL);
+
+  const { result, carry } = rotateLeftCircular(ctx.memory.read(address));
+
+  ctx.memory.write(address, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, carry);
+
+  return 16;
+}
+
+export function rotateRightCircularRegister(
+  ctx: InstructionCtx,
+  register: Register
+) {
+  const { result, carry } = rotateRightCircular(ctx.regs.read(register));
+
+  ctx.regs.write(register, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, carry);
+
+  return 8;
+}
+
+export function rotateRightCircularIndirectHL(ctx: InstructionCtx) {
+  const address = ctx.regs.readPair(RegisterPair.HL);
+
+  const { result, carry } = rotateRightCircular(ctx.memory.read(address));
+
+  ctx.memory.write(address, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, carry);
+
+  return 16;
+}
+
+export function rotateLeftRegister(ctx: InstructionCtx, register: Register) {
+  const { result, carry } = rotateLeft(
+    ctx.regs.read(register),
+    ctx.regs.isFlagSet(Flag.CY)
+  );
+
+  ctx.regs.write(register, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, carry);
+
+  return 8;
+}
+
+export function rotateLeftIndirectHL(ctx: InstructionCtx) {
+  const address = ctx.regs.readPair(RegisterPair.HL);
+
+  const { result, carry } = rotateLeft(
+    ctx.memory.read(address),
+    ctx.regs.isFlagSet(Flag.CY)
+  );
+
+  ctx.memory.write(address, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, carry);
+
+  return 16;
+}
+
+export function rotateRightRegister(ctx: InstructionCtx, register: Register) {
+  const { result, carry } = rotateRight(
+    ctx.regs.read(register),
+    ctx.regs.isFlagSet(Flag.CY)
+  );
+
+  ctx.regs.write(register, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, carry);
+
+  return 8;
+}
+
+export function rotateRightIndirectHL(ctx: InstructionCtx) {
+  const address = ctx.regs.readPair(RegisterPair.HL);
+
+  const { result, carry } = rotateRight(
+    ctx.memory.read(address),
+    ctx.regs.isFlagSet(Flag.CY)
+  );
+
+  ctx.memory.write(address, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, carry);
+
+  return 16;
+}
+
+function shiftLeftArithmetic(value: number) {
+  return {
+    result: (value << 1) & 0xff,
+    carry: (value & 0x80) !== 0,
+  };
+}
+
+export function shiftLeftArithmeticRegister(
+  ctx: InstructionCtx,
+  register: Register
+) {
+  const { result, carry } = shiftLeftArithmetic(ctx.regs.read(register));
+
+  ctx.regs.write(register, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, carry);
+
+  return 8;
+}
+
+export function shiftLeftArithmeticIndirectHL(ctx: InstructionCtx) {
+  const address = ctx.regs.readPair(RegisterPair.HL);
+
+  const { result, carry } = shiftLeftArithmetic(ctx.memory.read(address));
+
+  ctx.memory.write(address, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, carry);
+
+  return 16;
+}
+
+function shiftRightArithmetic(value: number) {
+  return {
+    result: ((value >> 1) & 0xff) | (value & 0x80),
+    carry: (value & 0x01) !== 0,
+  };
+}
+
+export function shiftRightArithmeticRegister(
+  ctx: InstructionCtx,
+  register: Register
+) {
+  const { result, carry } = shiftRightArithmetic(ctx.regs.read(register));
+
+  ctx.regs.write(register, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, carry);
+
+  return 8;
+}
+
+export function shiftRightArithmeticIndirectHL(ctx: InstructionCtx) {
+  const address = ctx.regs.readPair(RegisterPair.HL);
+
+  const { result, carry } = shiftRightArithmetic(ctx.memory.read(address));
+
+  ctx.memory.write(address, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, carry);
+
+  return 16;
+}
+
+function shiftRightLogical(value: number) {
+  return {
+    result: (value >> 1) & 0xff,
+    carry: (value & 0x01) !== 0,
+  };
+}
+
+export function shiftRightLogicalRegister(
+  ctx: InstructionCtx,
+  register: Register
+) {
+  const { result, carry } = shiftRightLogical(ctx.regs.read(register));
+
+  ctx.regs.write(register, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, carry);
+
+  return 8;
+}
+
+export function shiftRightLogicalIndirectHL(ctx: InstructionCtx) {
+  const address = ctx.regs.readPair(RegisterPair.HL);
+
+  const { result, carry } = shiftRightLogical(ctx.memory.read(address));
+
+  ctx.memory.write(address, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, carry);
+
+  return 16;
+}
+
+function swapNibbles(value: number) {
+  return ((value & 0xf) << 4) | ((value >> 4) & 0xf);
+}
+
+export function swapNibblesRegister(ctx: InstructionCtx, register: Register) {
+  const result = swapNibbles(ctx.regs.read(register));
+
+  ctx.regs.write(register, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, false);
+
+  return 8;
+}
+
+export function swapNibblesIndirectHL(ctx: InstructionCtx) {
+  const address = ctx.regs.readPair(RegisterPair.HL);
+
+  const result = swapNibbles(ctx.memory.read(address));
+
+  ctx.memory.write(address, result);
+  ctx.regs.setFlag(Flag.Z, result === 0);
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, false);
+  ctx.regs.setFlag(Flag.CY, false);
+
+  return 16;
+}
+
+function testBit(value: number, bit: number) {
+  return !(value & (1 << bit));
+}
+
+export function testBitRegister(
+  ctx: InstructionCtx,
+  bit: number,
+  register: Register
+) {
+  ctx.regs.setFlag(Flag.Z, testBit(ctx.regs.read(register), bit));
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, true);
+
+  return 8;
+}
+
+export function testBitIndirectHL(ctx: InstructionCtx, bit: number) {
+  const address = ctx.regs.readPair(RegisterPair.HL);
+
+  ctx.regs.setFlag(Flag.Z, testBit(ctx.memory.read(address), bit));
+  ctx.regs.setFlag(Flag.N, false);
+  ctx.regs.setFlag(Flag.H, true);
+
+  return 12;
+}
+
+function resetBit(value: number, bit: number) {
+  return value & ~(1 << bit);
+}
+
+export function resetBitRegister(
+  ctx: InstructionCtx,
+  bit: number,
+  register: Register
+) {
+  ctx.regs.write(register, resetBit(ctx.regs.read(register), bit));
+  return 8;
+}
+
+export function resetBitIndirectHL(ctx: InstructionCtx, bit: number) {
+  const address = ctx.regs.readPair(RegisterPair.HL);
+  ctx.memory.write(address, resetBit(ctx.memory.read(address), bit));
+  return 16;
+}
+
+function setBit(value: number, bit: number) {
+  return value | (1 << bit);
+}
+
+export function setBitRegister(
+  ctx: InstructionCtx,
+  bit: number,
+  register: Register
+) {
+  ctx.regs.write(register, setBit(ctx.regs.read(register), bit));
+  return 8;
+}
+
+export function setBitIndirectHL(ctx: InstructionCtx, bit: number) {
+  const address = ctx.regs.readPair(RegisterPair.HL);
+  ctx.memory.write(address, setBit(ctx.memory.read(address), bit));
+  return 16;
 }
